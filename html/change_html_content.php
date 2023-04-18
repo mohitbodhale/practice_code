ee59fb506a9ba4c94a6b9c87a0f0865dabe86b4d
mohit.bodhale <mohit.bodhale@pitonsystems.com>
2023-04-19 00:35:42 +0530

    added

---
+++ html/change_html_content.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+<select name="city" id="city">
+    <option value="kopargaon">kopargaon</option>
+    <option value="Pune">Pune</option>
+</select>
+
+<script>
+    
+</script>
+
+</body>
+
+<!-- Mirrored from www.w3schools.com/js/tryit.asp?filename=tryjs_intro_inner_html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 19 Feb 2019 07:47:12 GMT -->
+</html>
---
+++ html/insatallation problem.php
@@ -0,0 +1,6 @@
+
+
+    "post-autoload-dump" : "Cake\\Composer\\Installer\\PluginInstaller::postAutoloadDump",
+app.php
+    'Error' => [
+        'errorLevel' => 'E_ALL & ~E_USER_DEPRECATED',
