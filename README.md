cf039a3b460b83f0f7ddf161411ad748062fbd3c
mohit.bodhale <mohit.bodhale@pitonsystems.com>
2023-04-19 00:44:20 +0530

    add

--- README.md
+++
@@ -1,2 +0,0 @@
-# practice_code
-practical_code
--- html/insatallation problem.php
+++
@@ -1,6 +0,0 @@
-
-
-    "post-autoload-dump" : "Cake\\Composer\\Installer\\PluginInstaller::postAutoloadDump",
-app.php
-    'Error' => [
-        'errorLevel' => 'E_ALL & ~E_USER_DEPRECATED',
